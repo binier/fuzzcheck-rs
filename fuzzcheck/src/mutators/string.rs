036e6388e1419c1c43f9b6b6eb1c7446c0f3a8fd
Zura Benashvili <zura.benashvili@gmail.com>
2021-11-18 18:04:39 +0400

    feat(mutators/string): add mutator for tezos ChainName

--- fuzzcheck/src/mutators/string.rs
+++ fuzzcheck/src/mutators/string.rs
@@ -1,5 +1,7 @@
-use crate::DefaultMutator;
+use crate as fuzzcheck;
+use crate::{DefaultMutator, ExtendedMutator, MutatorValueConverter, MutatorWrapper};
 
+use super::enums::BasicEnumMutator;
 use super::unit::UnitMutator;
 
 impl DefaultMutator for String {
@@ -7,5 +9,61 @@ impl DefaultMutator for String {
 
     fn default_mutator() -> Self::Mutator {
         UnitMutator::new(String::new())
+    }
+}
+
+#[derive(DefaultMutator, Clone)]
+pub enum ChainName {
+    Mainnet,
+}
+
+pub struct ChainNameToStringConverter {}
+
+impl ChainNameToStringConverter {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl MutatorValueConverter for ChainNameToStringConverter {
+    type InnerValue = ChainName;
+    type Value = String;
+
+    fn from_inner_value_ref(&self, inner_value: &Self::InnerValue) -> Self::Value {
+        match inner_value {
+            ChainName::Mainnet => "TEZOS_MAINNET".to_owned(),
+        }
+    }
+
+    fn to_inner_value_ref(&self, value: &Self::Value) -> Self::InnerValue {
+        match value.as_str() {
+            "TEZOS_MAINNET" => ChainName::Mainnet,
+            _ => unreachable!(),
+        }
+    }
+}
+
+pub type ChainNameStringMutatorInner = ExtendedMutator<String, ChainName, BasicEnumMutator, ChainNameToStringConverter>;
+
+pub struct ChainNameStringMutator {
+    inner: ChainNameStringMutatorInner,
+}
+
+impl ChainNameStringMutator {
+    pub fn new() -> Self {
+        Self {
+            inner: ChainNameStringMutatorInner::new(
+                BasicEnumMutator::new::<ChainName>(),
+                ChainNameToStringConverter::new(),
+            ),
+        }
+    }
+}
+
+impl MutatorWrapper for ChainNameStringMutator {
+    type Wrapped = ChainNameStringMutatorInner;
+
+    fn wrapped_mutator(&self) -> &Self::Wrapped {
+        &self.inner
     }
 }
