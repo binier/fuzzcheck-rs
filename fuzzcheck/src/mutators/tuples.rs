844d739b257cf42c44f3754077311bc71b462d45
Daniel Kuehr <daniel.kuehr@viablesystems.io>
2022-02-01 15:36:36 -0300

    add support for larger tuples

--- fuzzcheck/src/mutators/tuples.rs
+++ fuzzcheck/src/mutators/tuples.rs
@@ -683,3 +683,23 @@ mod tuple10 {
     extern crate self as fuzzcheck;
     fuzzcheck_mutators_derive::make_basic_tuple_mutator!(10);
 }
+pub use tuple11::{Tuple11, Tuple11Mutator};
+mod tuple11 {
+    extern crate self as fuzzcheck;
+    fuzzcheck_mutators_derive::make_basic_tuple_mutator!(11);
+}
+pub use tuple12::{Tuple12, Tuple12Mutator};
+mod tuple12 {
+    extern crate self as fuzzcheck;
+    fuzzcheck_mutators_derive::make_basic_tuple_mutator!(12);
+}
+pub use tuple13::{Tuple13, Tuple13Mutator};
+mod tuple13 {
+    extern crate self as fuzzcheck;
+    fuzzcheck_mutators_derive::make_basic_tuple_mutator!(13);
+}
+pub use tuple14::{Tuple14, Tuple14Mutator};
+mod tuple14 {
+    extern crate self as fuzzcheck;
+    fuzzcheck_mutators_derive::make_basic_tuple_mutator!(14);
+}
